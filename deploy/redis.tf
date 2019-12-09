07a63f78fc80f08a0ef724d8cdcc35ba1d4454f1
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-12-09 18:07:03 -0500

    Update redis.tf

--- deploy/redis.tf
+++ deploy/redis.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_subnet_group" "subnet_group" {
 }
 
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${var.repo_name}-${var.branch-name}"
+  cluster_id           = "${var.repo_name}-${var.branch_name}"
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
