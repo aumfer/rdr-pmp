6ead18d2dea6b0018117a71f7dab24ebeb0ba266
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-12-12 22:34:08 -0500

    cache policy

--- deploy/redis.tf
+++ deploy/redis.tf
@@ -3,11 +3,23 @@ resource "aws_elasticache_subnet_group" "subnet_group" {
   subnet_ids = data.aws_subnet_ids.subnets.ids
 }
 
+resource "aws_elasticache_parameter_group" "default" {
+  name   = "${var.repo_name}-${var.branch_name}"
+  family = "redis5.0"
+
+  parameter {
+    name  = "maxmemory-policy"
+    value = "allkeys-lru"
+  }
+}
+
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "${var.repo_name}-${var.branch_name}"
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
+  parameter_group_name = aws_elasticache_parameter_group.default.id
+  apply_immediately    = true
 
   security_group_ids = [aws_security_group.redis_security_group.id]
   subnet_group_name = aws_elasticache_subnet_group.subnet_group.name
